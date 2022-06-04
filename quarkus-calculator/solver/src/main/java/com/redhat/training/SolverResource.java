43b44b51046f94a4ba1fd2bf030a208175552b7e
Tom Berrey <tberrey@redhat.com>
2022-06-03 21:54:46 -0600

    Updated pom.xmls, application.properties and SolverResource.java

--- quarkus-calculator/solver/src/main/java/com/redhat/training/SolverResource.java
+++ quarkus-calculator/solver/src/main/java/com/redhat/training/SolverResource.java
@@ -16,8 +16,16 @@ import com.redhat.training.service.SolverService;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import io.jaegertracing.internal.JaegerSpanContext;
+import io.opentracing.SpanContext;
+import io.opentracing.Tracer;
+import io.smallrye.opentracing.contrib.resolver.TracerResolver;
+
 public class SolverResource implements SolverService {
     final Logger log = LoggerFactory.getLogger(SolverResource.class);
+
+    @Inject
+    Tracer tracer;
 
     @Inject
     @RestClient
@@ -36,17 +44,37 @@ public class SolverResource implements SolverService {
     @Produces(MediaType.TEXT_PLAIN)
     public Float solve(@PathParam("equation") String equation) {
         log.info("Solving '{}'", equation);
+
+        String traceId = "none";
+
+        final SpanContext spanContext = tracer.scopeManager().activeSpan().context();
+        if (spanContext instanceof JaegerSpanContext) {
+            traceId = ((JaegerSpanContext) spanContext).getTraceId();
+        }
+
+        TracerResolver.resolveTracer().activeSpan().setTag("traceId", traceId);
+        TracerResolver.resolveTracer().activeSpan().setTag("tagMessage", "this is an awesome message");
+
         try {
             return Float.valueOf(equation);
         } catch (NumberFormatException e) {
             Matcher addMatcher = addPattern.matcher(equation);
             if (addMatcher.matches()) {
-                return adderService.add(addMatcher.group(1), addMatcher.group(2));
+
+                Float result = adderService.add(addMatcher.group(1), addMatcher.group(2));
+                TracerResolver.resolveTracer().activeSpan().setBaggageItem("answer", result + " = " + result);
+                return result;
+            
             }
             Matcher multiplyMatcher = multiplyPattern.matcher(equation);
             if (multiplyMatcher.matches()) {
-                return multiplierService.multiply(multiplyMatcher.group(1), multiplyMatcher.group(2));
+
+                Float result = multiplierService.multiply(multiplyMatcher.group(1), multiplyMatcher.group(2));
+                TracerResolver.resolveTracer().activeSpan().setBaggageItem("answer", result + " = " + result);
+                return result;
+            
             } else {
+                TracerResolver.resolveTracer().activeSpan().setBaggageItem("answer", "Unable to parse: " + equation);
                 throw new WebApplicationException(
                         Response.status(Response.Status.BAD_REQUEST).entity("Unable to parse: " + equation).build());
             }
