7cda9f6e36dd734512f485ec5c3846b4e6287cc0
Tom Berrey <tberrey@redhat.com>
2022-06-21 11:05:02 -0600

    Updated start-remotely.sh to deploy Solver microservice last

--- quarkus-calculator/start-remotely.sh
+++ quarkus-calculator/start-remotely.sh
@@ -3,23 +3,28 @@
 oc new-project calculator
 oc project calculator
 
-echo "Starting the 'solver' project "
-cd solver
-mvn package -Dquarkus.container-image.push=true
-oc apply -f target/kubernetes/openshift.yml
-cd ..
-
 echo "Starting the 'adder' project "
 cd adder
 mvn package -Dquarkus.container-image.push=true
+oc delete -f target/kubernetes/openshift.yml
 oc apply -f target/kubernetes/openshift.yml
 cd ..
 
 echo "Starting the 'multiplier' project "
 cd multiplier
 mvn package -Dquarkus.container-image.push=true
+oc delete -f target/kubernetes/openshift.yml
 oc apply -f target/kubernetes/openshift.yml
 cd ..
+
+echo "Starting the 'solver' project "
+cd solver
+mvn package -Dquarkus.container-image.push=true
+oc delete -f target/kubernetes/openshift.yml
+oc apply -f target/kubernetes/openshift.yml
+cd ..
+
+sleep 10
 
 EQUATION="5*4+3"
 SOLVER_ROUTE=$(oc get route solver -o template --template '{{ "http://" }}{{ .spec.host }}')
