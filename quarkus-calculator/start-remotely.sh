b44b8ada9569392ab4c4265301572e1996dcbfb5
EC2 Default User <ec2-user@ip-172-31-7-237.us-west-1.compute.internal>
2022-07-21 15:36:18 +0000

    Added NATS Server install in OpenShift

--- quarkus-calculator/start-remotely.sh
+++ quarkus-calculator/start-remotely.sh
@@ -10,26 +10,30 @@ oc apply -f jaeger-resource.yml
 oc delete -f activemq-resource.yml
 oc apply -f activemq-resource.yml
 
+helm uninstall nats
+helm repo add nats https://nats-io.github.io/k8s/helm/charts/
+helm install nats nats/nats --values nats_ha_and_jetstream.yml
+
 echo "Starting the 'adder' project"
 cd adder
-mvn clean
-mvn package -Dquarkus.container-image.push=true
+#mvn clean
+#mvn package -Dquarkus.container-image.push=true
 oc delete -f target/kubernetes/openshift.yml
 oc apply -f target/kubernetes/openshift.yml
 cd ..
 
 echo "Starting the 'multiplier' project"
 cd multiplier
-mvn clean
-mvn package -Dquarkus.container-image.push=true
+#mvn clean
+#mvn package -Dquarkus.container-image.push=true
 oc delete -f target/kubernetes/openshift.yml
 oc apply -f target/kubernetes/openshift.yml
 cd ..
 
 echo "Starting the 'solver' project"
 cd solver
-mvn clean
-mvn package -Dquarkus.container-image.push=true
+#mvn clean
+#mvn package -Dquarkus.container-image.push=true
 oc delete -f target/kubernetes/openshift.yml
 oc apply -f target/kubernetes/openshift.yml
 cd ..
