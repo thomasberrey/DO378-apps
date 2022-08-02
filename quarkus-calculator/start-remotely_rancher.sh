5b29bf888348e467ef891dcfa028d7d380254829
Tom Berrey <tberrey@redhat.com>
2022-08-02 17:15:16 -0500

    Added installation of Jaeger

--- quarkus-calculator/start-remotely_rancher.sh
+++ quarkus-calculator/start-remotely_rancher.sh
@@ -1,44 +1,32 @@
 #!/bin/bash
 
-kubectl create namespace calculator
-kubectl config set-context --current --namespace=calculator
+NAMESPACE=calculator
+
+kubectl create namespace $NAMESPACE
+kubectl config set-context --current --namespace=$NAMESPACE
 kubectl delete all --all
+
+helm uninstall jaegertracing
+helm repo add jaegertracing https://jaegertracing.github.io/helm-charts
+helm install jaegertracing jaegertracing/jaeger
+
+export JAEGER_POD_NAME=$(kubectl get pods --namespace $NAMESPACE -l "app.kubernetes.io/instance=jaegertracing,app.kubernetes.io/component=query" -o jsonpath="{.items[0].metadata.name}")
+
+JAEGER_PODMAN_STATUS=`kubectl get pod $JAEGER_POD_NAME | grep $JAEGER_POD_NAME | awk '{print $3}'`
+
+while [ $JAEGER_PODMAN_STATUS != "Running" ]
+do
+  JAEGER_PODMAN_STATUS=`kubectl get pod $JAEGER_POD_NAME | grep $JAEGER_POD_NAME | awk '{print $3}'`
+  DATE=`date`
+  echo "$DATE - JAEGER_PODMAN_STATUS: $JAEGER_PODMAN_STATUS"
+  sleep 5
+done
+
+kubectl port-forward --namespace $NAMESPACE $JAEGER_POD_NAME 8080:16686 &
 
 helm uninstall nats
 helm repo add nats https://nats-io.github.io/k8s/helm/charts/
 helm install nats nats/nats --values nats_ha_and_jetstream.yml
 
-
-
-
 #kubectl port-forward -n kube-system $(kubectl -n kube-system get pods --selector "app.kubernetes.io/name=traefik" --output=name) 9000:9000
 
-#oc delete -f jaeger-resource.yml
-#oc apply -f jaeger-resource.yml
-
-#oc delete -f activemq-resource.yml
-#oc apply -f activemq-resource.yml
-
-#echo "Starting the 'adder' project"
-#cd adder
-#mvn clean
-#mvn package -Dquarkus.container-image.push=true
-#oc delete -f target/kubernetes/openshift.yml
-#oc apply -f target/kubernetes/openshift.yml
-#cd ..
-
-#echo "Starting the 'multiplier' project"
-#cd multiplier
-#mvn clean
-#mvn package -Dquarkus.container-image.push=true
-#oc delete -f target/kubernetes/openshift.yml
-#oc apply -f target/kubernetes/openshift.yml
-#cd ..
-
-#echo "Starting the 'solver' project"
-#cd solver
-#mvn clean
-#mvn package -Dquarkus.container-image.push=true
-#oc delete -f target/kubernetes/openshift.yml
-#oc apply -f target/kubernetes/openshift.yml
-#cd ..
