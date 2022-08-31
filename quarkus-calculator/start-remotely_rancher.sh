d2ff80d9e2f62c006b968628dd77c0c802bb4f2a
Tom Berrey <tberrey@redhat.com>
2022-08-31 09:15:21 -0500

    Updated microservices deployment files and Rancher deployments

--- quarkus-calculator/start-remotely_rancher.sh
+++ quarkus-calculator/start-remotely_rancher.sh
@@ -8,15 +8,8 @@ kubectl delete all --all
 
 curl -sL https://github.com/operator-framework/operator-lifecycle-manager/releases/download/v0.21.2/install.sh | bash -s v0.21.2
 
-kubectl create -f https://operatorhub.io/install/elastic-cloud-eck.yaml
-kubectl apply -f elasticsearch-resource.yml
-
 kubectl create -f https://operatorhub.io/install/jaeger.yaml
-#kubectl apply -f jaeger-resource.yml
-
-helm uninstall activemq-artemis
-helm repo add activemq-artemis https://deviceinsight.github.io/activemq-artemis-helm
-helm install activemq-artemis activemq-artemis/activemq-artemis
+kubectl apply -f jaeger-resource.yml
 
 helm uninstall nats
 helm repo add nats https://nats-io.github.io/k8s/helm/charts/
@@ -25,29 +18,16 @@ helm install nats nats/nats --values nats_ha_and_jetstream.yml
 helm uninstall nginx
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm install nginx bitnami/nginx
+kubectl apply -f nginx_ingress.yml
 
 kubectl create -f https://operatorhub.io/install/rabbitmq-cluster-operator.yaml
 kubectl apply -f rabbitmq-resource.yml
 
+kubectl apply -k "github.com/kubernetes-sigs/gateway-api/config/crd?ref=v0.4.0"
+helm uninstall traefik
+helm repo add traefik https://helm.traefik.io/traefik
+helm install traefik --set experimental.kubernetesGateway.enabled=true traefik/traefik
 
-
-#helm uninstall jaegertracing
-#helm repo add jaegertracing https://jaegertracing.github.io/helm-charts
-#helm install jaegertracing jaegertracing/jaeger
-
-#export JAEGER_POD_NAME=$(kubectl get pods --namespace $NAMESPACE -l "app.kubernetes.io/instance=jaegertracing,app.kubernetes.io/component=query" -o jsonpath="{.items[0].metadata.name}")
-
-#JAEGER_PODMAN_STATUS=`kubectl get pod $JAEGER_POD_NAME | grep $JAEGER_POD_NAME | awk '{print $3}'`
-
-#while [ $JAEGER_PODMAN_STATUS != "Running" ]
-#do
-#  JAEGER_PODMAN_STATUS=`kubectl get pod $JAEGER_POD_NAME | grep $JAEGER_POD_NAME | awk '{print $3}'`
-#  DATE=`date`
-#  echo "$DATE - Waiting for Jaeger Query Service to start... Status: $JAEGER_PODMAN_STATUS"
-#  sleep 5
-#done
-
-#kubectl port-forward --namespace $NAMESPACE $JAEGER_POD_NAME 8080:16686 &
-
-#kubectl port-forward -n kube-system $(kubectl -n kube-system get pods --selector "app.kubernetes.io/name=traefik" --output=name) 9000:9000
-
+kubectl apply -f adder_kubernetes.yml
+kubectl apply -f multiplier_kubernetes.yml
+kubectl apply -f solver_kubernetes.yml
